e7026faf1a9e72227fdfb4d034ff981103ae73c1
naeem <F2017065312@umt.edu.pk>
2021-05-29 10:39:08 +0500

    .

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -2,83 +2,93 @@ import 'package:flutter/material.dart';
 import 'package:grocery_item_app/screens/onboard.dart';
 import 'package:grocery_item_app/screens/todolist.dart';
 
-class Welcome extends StatelessWidget {
+class Welcome extends StatefulWidget {
+  @override
+  _WelcomeState createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
   bool _validPhone = false;
+
   void showBottomSheet(context) {
     showModalBottomSheet(
       context: context,
       builder: (context) => StatefulBuilder(
         builder: (context, StateSetter mystate) {
-          return Container(
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    'LOGIN',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    'Enter Your Phone Number to Login',
-                    style: TextStyle(fontSize: 12, color: Colors.grey),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      prefixText: '+91',
-                      labelText: '10 digit Mobile Number',
+          return SingleChildScrollView(
+            child: Container(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'LOGIN',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                     ),
-                    autofocus: true,
-                    keyboardType: TextInputType.phone,
-                    maxLength: 10,
-                    onChanged: (value) {
-                      if (value.length == 10) {
-                        mystate(() {
-                          _validPhone = true;
-                        });
-                      } else {
-                        mystate(() {
-                          _validPhone = false;
-                        });
-                      }
-                    },
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: AbsorbPointer(
-                          absorbing: _validPhone ? false : true,
-                          child: TextButton(
-                            style: ButtonStyle(
-                              backgroundColor: MaterialStateProperty.all<Color>(
-                                  _validPhone
-                                      ? Colors.deepOrange
-                                      : Colors.grey),
-                              foregroundColor: MaterialStateProperty.all<Color>(
-                                  Colors.white),
+                    Text(
+                      'Enter Your Phone Number to Login',
+                      style: TextStyle(fontSize: 12, color: Colors.grey),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        prefixText: '+91',
+                        labelText: '10 digit Mobile Number',
+                      ),
+                      autofocus: true,
+                      keyboardType: TextInputType.phone,
+                      maxLength: 10,
+                      onChanged: (value) {
+                        if (value.length == 10) {
+                          mystate(() {
+                            _validPhone = true;
+                          });
+                        } else {
+                          mystate(() {
+                            _validPhone = false;
+                          });
+                        }
+                      },
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: AbsorbPointer(
+                            absorbing: _validPhone ? false : true,
+                            child: TextButton(
+                              style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.all<Color>(_validPhone
+                                        ? Colors.deepOrange
+                                        : Colors.grey),
+                                foregroundColor:
+                                    MaterialStateProperty.all<Color>(
+                                        Colors.white),
+                              ),
+                              onPressed: () {
+                                if (_validPhone) {
+                                  Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => TodoList()));
+                                }
+                              },
+                              child: Text(
+                                  _validPhone ? 'CONTINUE' : 'Enter Phone'),
                             ),
-                            onPressed: () {
-                              if (_validPhone) {
-                                Navigator.pushReplacement(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => TodoList()));
-                              }
-                            },
-                            child:
-                                Text(_validPhone ? 'CONTINUE' : 'Enter Phone'),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           );
